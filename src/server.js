be0af9ac6157ec51f62bd3aacea4603ec38e38ca
gwfernandez <gwfernandez@gmail.com>
2019-07-08 23:26:30 -0300

    configuracion para deploy en heroku

--- src/server.js
+++ src/server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
 require('./config/hbs/helpers');
- 
+
+const port = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'hbs');
 app.set('views',path.join(__dirname,'/views'))
 app.set('partials',path.join(__dirname,'/views/partials'))
-app.listen(3000, () => {
-    console.log('Express web-server started on port 3000 ...');
+app.listen(port, () => {
+    console.log(`Express web-server started on port ${port} ...`);
 });
 
 hbs.registerPartials(__dirname + '/views/partials');
